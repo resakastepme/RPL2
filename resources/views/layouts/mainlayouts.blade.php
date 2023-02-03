a51852ef91eafb3a4c2d3fe4fc13c4fcb9921857
resakastepme <88456993+resakastepme@users.noreply.github.com>
2023-02-03 17:43:00 +0700

    ni di

--- resources/views/layouts/mainlayouts.blade.php
+++ resources/views/layouts/mainlayouts.blade.php
@@ -23,15 +23,15 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
 
     <!-- Libraries Stylesheet -->
-    <link href="lib/animate/animate.min.css" rel="stylesheet">
-    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
-    <link href="lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css" rel="stylesheet" />
+    <link href="{{ asset ('lib/animate/animate.min.css') }}" rel="stylesheet">
+    <link href="{{ asset ('lib/owlcarousel/assets/owl.carousel.min.css') }}" rel="stylesheet">
+    <link href="{{ asset ('lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css') }}" rel="stylesheet" />
 
     <!-- Customized Bootstrap Stylesheet -->
     <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
 
     <!-- Template Stylesheet -->
-    <link href="css/style.css" rel="stylesheet">
+    <link href="{{ asset ('css/style.css') }}" rel="stylesheet">
 </head>
 
 <body>
@@ -44,8 +44,6 @@
             </div>
         </div>
         <!-- Spinner End -->
-
-        @yield('content')
 
 
         <!-- Navbar & Hero Start -->
@@ -99,7 +97,7 @@
         </div>
         <!-- Navbar & Hero End -->
 
-
+        @yield('content')
 
         <!-- Back to Top -->
         <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
@@ -108,17 +106,17 @@
     <!-- JavaScript Libraries -->
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="lib/wow/wow.min.js"></script>
-    <script src="lib/easing/easing.min.js"></script>
-    <script src="lib/waypoints/waypoints.min.js"></script>
-    <script src="lib/counterup/counterup.min.js"></script>
-    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
-    <script src="lib/tempusdominus/js/moment.min.js"></script>
-    <script src="lib/tempusdominus/js/moment-timezone.min.js"></script>
-    <script src="lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js"></script>
+    <script src="{{ asset ('lib/wow/wow.min.js') }}"></script>
+    <script src="{{ asset ('lib/easing/easing.min.js') }}"></script>
+    <script src="{{ asset ('lib/waypoints/waypoints.min.js') }}"></script>
+    <script src="{{ asset ('lib/counterup/counterup.min.js') }}"></script>
+    <script src="{{ asset ('lib/owlcarousel/owl.carousel.min.js') }}"></script>
+    <script src="{{ asset ('lib/tempusdominus/js/moment.min.js') }}"></script>
+    <script src="{{ asset ('lib/tempusdominus/js/moment-timezone.min.js') }}"></script>
+    <script src="{{asset('lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js') }}"></script>
 
     <!-- Template Javascript -->
-    <script src="js/main.js"></script>
+    <script src="{{ asset('js/main.js') }}"></script>
 </body>
 
 </html>
