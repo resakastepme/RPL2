fd899ad7c80135a112387b6291b165443186a133
nasikuningdewa <didisippo.ds@gmail.com>
2023-02-03 21:51:02 +0700

    gantiin warnanya res

--- resources/views/layouts/mainlayouts.blade.php
+++ resources/views/layouts/mainlayouts.blade.php
@@ -76,7 +76,7 @@
                 </div>
             </nav>
 
-            <div class="container-xxl py-5 bg-dark hero-header mb-5">
+            <div class="container-xxl py-5 bg-dark hero-header  mb-5">
                 <div class="container my-5 py-5">
                     <div class="row align-items-center g-5">
                         <div class="col-lg-6 text-center text-lg-start">
